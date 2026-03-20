18ff325c54d7796677ebadded61c89dbaead5c00
NoScoBoe <boedeckern.25@william.jewell.edu>
2026-03-20 16:50:42 -0500

    Created Q2 and Q3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,42 @@
 console.log("script.js connected!");
-
-let buttons = document.querySelectorAll("#question-1 button")
 let userAnswers={}
-buttons.forEach(function(button){
+
+let buttons1 = document.querySelectorAll("#question-1 button")
+buttons1.forEach(function(button){
     button.addEventListener("click",function(){
-        buttons.forEach(function(btn){
+        buttons1.forEach(function(btn){
+            btn.classList.remove("selected")
+        })
+        button.classList.add("selected")
+
+        let buttonID=button.dataset.buttonid
+        let answer=button.dataset.answer
+        userAnswers[buttonID]=answer
+        console.log(userAnswers)
+
+    })
+})
+
+let buttons2 = document.querySelectorAll("#question-2 button")
+buttons2.forEach(function(button){
+    button.addEventListener("click",function(){
+        buttons2.forEach(function(btn){
+            btn.classList.remove("selected")
+        })
+        button.classList.add("selected")
+
+        let buttonID=button.dataset.buttonid
+        let answer=button.dataset.answer
+        userAnswers[buttonID]=answer
+        console.log(userAnswers)
+
+    })
+})
+
+let buttons3 = document.querySelectorAll("#question-3 button")
+buttons3.forEach(function(button){
+    button.addEventListener("click",function(){
+        buttons3.forEach(function(btn){
             btn.classList.remove("selected")
         })
         button.classList.add("selected")
