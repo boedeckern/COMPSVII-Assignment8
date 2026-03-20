c89b7c2f85cd88d7ac431cf520a691130d8d743f
NoScoBoe <boedeckern.25@william.jewell.edu>
2026-03-20 16:42:13 -0500

    Created Q1 and answer storage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("script.js connected!");
+
+let buttons = document.querySelectorAll("#question-1 button")
+let userAnswers={}
+buttons.forEach(function(button){
+    button.addEventListener("click",function(){
+        buttons.forEach(function(btn){
+            btn.classList.remove("selected")
+        })
+        button.classList.add("selected")
+
+        let buttonID=button.dataset.buttonid
+        let answer=button.dataset.answer
+        userAnswers[buttonID]=answer
+        console.log(userAnswers)
+
+    })
+})
